803cda579aa35d066559254febfa38cc018a6854
MatiasTontti <matias.tontti@gmail.com>
2017-09-05 15:59:42 +0300

    initial commit

--- app/src/main/java/com/example/matias/menubarmvp/MenuBar.java
+++ app/src/main/java/com/example/matias/menubarmvp/MenuBar.java
@@ -1,5 +1,7 @@
 package com.example.matias.menubarmvp;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -79,14 +81,23 @@ public class MenuBar extends AppCompatActivity
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
+        FragmentManager fragmentManager = getFragmentManager();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
+        if (id == R.id.nav_first_layout) {
+            fragmentManager.beginTransaction()
+                    .replace(R.id.content_frame
+                            , new FirstFragment())
+                    .commit();
+        } else if (id == R.id.nav_second_layout) {
+            fragmentManager.beginTransaction()
+                    .replace(R.id.content_frame
+                            , new SecondFragment())
+                    .commit();
+        } else if (id == R.id.nav_third_layout) {
+            fragmentManager.beginTransaction()
+                    .replace(R.id.content_frame
+                            , new ThirdFragment())
+                    .commit();
 
         } else if (id == R.id.nav_share) {
 
